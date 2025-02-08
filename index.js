9f11f556f8a76a250e6e1b16ad416073ef5e55b1
unnatiirawat <140919232+unnatiirawat@users.noreply.github.com>
2025-02-08 21:32:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// que 1
+var a1 = 2;
+var b1 = 5;
+var add = a1+b1;
+console.log(add); 
+
+
+// que 2
+
+let x1=5, x2= 3; 
+
+if(x1<10<10 && x1>x2)
+{console.log(true);
+}
+else{
+    console.log(false);
+    
+}
+
+
+//que 3
+
+     var A = 12; B = 20;
+    console.log(A % 10 === 0 || B % 10 === 0);
+  
+// que 4
+
+   var n = 4567;
+  
+   var result = Math.floor(n/1000);
+    console.log(result);
+
+
+    // que 5.Find the last digit of a 4 digit number
+
+    var x = 4567;
+    var res = x % 10;
+    console.log(res);
+
+    // Q6.Find the remainder
+
+    var s = 9 , t= 2;
+    var rem = (s % t) ;
+    console.log(rem);
+
+    //Q7.Multiply two Numbers
+
+    var a = 2, b = 5;
+    var multi = (a*b);
+    console.log(multi);
+
+    //Q8. Marks Calculator
+
+    var a = 50 , b = 20, c = 100;
+    var sum = (a+b+c);
+    var average = ((a + b + c)/3);
+    console.log(sum , average);
+    
+    
+
+    
+   
